c9bf8442ec5117996192b75db6e2d5f346de9623
Alyona <alozovska1504@gmail.com>
2022-01-18 23:05:26 +0200

    add files

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,24 +1,14 @@
 export function fetchCountries(name) {
-    const url = `https://restcountries.com/v3.1/name/${name}'?fields=name,capital,population,flags,languages`;
-    
-    return fetch(url).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    });
+  const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
 
-
-// async function fetchCountries(name) {
-//     const url = `https://restcountries.com/v3.1/name/${name}'?fields=name,capital,population,flags,languages`;
-    
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//         throw new Error(response.status);
-//     }
-//     return await response.json();
-// }
 
 
 
