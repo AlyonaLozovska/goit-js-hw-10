8b1bb84f6b678e77ba548e942a0fbe6bf63296f9
Alyona <alozovska1504@gmail.com>
2022-01-18 02:16:06 +0200

    add files

---
+++ src/fetchCountries.js
@@ -0,0 +1,48 @@
+export function fetchCountries(name) {
+    const url = `https://restcountries.com/v3.1/name/${name}'?fields=name,capital,population,flags,languages`;
+    
+    return fetch(url).then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    });
+}
+
+
+// async function fetchCountries(name) {
+//     const url = `https://restcountries.com/v3.1/name/${name}'?fields=name,capital,population,flags,languages`;
+    
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//         throw new Error(response.status);
+//     }
+//     return await response.json();
+// }
+
+
+
+
+
+
+
+
+
+
+
+// Тебе нужны только следующие свойства:
+
+// name.official - полное имя страны
+// capital - столица
+// population - население
+// flags.svg - ссылка на изображение флага
+// languages - массив языков
+
+// https://restcountries.com/v3.1/name/{name}?fullText=true
+// https://restcountries.com/v3.1/capital/{capital}
+
+// "flags": {
+//   "svg": "https://restcountries.com/data/per.svg",
+//   "png": "https://restcountries.com/data/png/per.png"
+// }
+// https://restcountries.com/v3.1/lang/{lang}
